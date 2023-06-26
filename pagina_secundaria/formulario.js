28711398b334970ce91e7ae9d1017b754f530ac3
Carlos <carlosvivanco410@gmail.com>
2023-06-26 08:36:42 -0600

    carpeta

---
+++ pagina_secundaria/formulario.js
@@ -0,0 +1,52 @@
+const formulario = documento.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input')
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const validarFormulario = (e) => {
+    switch(e.target.name){
+        case "usuario":
+            if (expresiones.usuario.test(e.target.value)){
+
+            }else{
+                document.getElementById
+            }
+            console.log('Funcional');
+        break
+
+        case "nombre":
+            console.log('Funcional');
+        break
+
+        case "password":
+            console.log('Funcional');
+        break
+
+        case "password2":
+            console.log('Funcional');
+        break
+
+        case "correo":
+            console.log('Funcional');
+        break
+
+        case "telefono":
+            console.log('Funcional');
+        break
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
